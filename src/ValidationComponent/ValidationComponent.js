d44dfdfc1a7bb718c7c2984000e67f17d5edc7fd
petercolella <peter.joseph.colella@gmail.com>
2018-06-18 00:00:05 -0400

    Step 3

--- src/ValidationComponent/ValidationComponent.js
+++ src/ValidationComponent/ValidationComponent.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 const validationComponent = (props) => {
+    if (props.length < 5) {
     return (
         <div>
-            <p>{props.length}</p>
+         <p>Text too short</p>
         </div>
     )
+} else {
+    return (
+        <div>
+            <p>Text long enough</p>
+        </div>
+    )
+}
 }
 
 export default validationComponent;
